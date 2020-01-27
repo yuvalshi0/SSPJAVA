8839bb00c984e776834479b7fe05e135c8f9c817
Yuval Shimon <yuvalshi0@gmail.com>
2020-01-27 22:21:35 +0200

    fixed server tester

--- ServerTester/src/Request.java
+++ ServerTester/src/Request.java
@@ -1,18 +1,20 @@
 
 import java.util.Map;
 
+import com.google.gson.JsonObject;
+
 public class Request<T> {
 	private Map<String, String> headers;
-	private T content;
+	private String content;
 	
 	
-	public Request(Map<String, String> headers, T content) {
+	public Request(Map<String, String> headers, String content) {
 		super();
 		this.headers = headers;
 		this.content = content;
 	}
 
-	public T getContent() {
+	public String getContent() {
 		return content;
 	}
 	
